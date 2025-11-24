ce80181fca711cc5a430c0d26c8dbcaa363a9390
Tselot Million <tselotmillion8@gmail.com>
2025-11-24 19:00:42 +0300

    add swagger documentation

--- alx_project_nexus/urls.py
+++ alx_project_nexus/urls.py
@@ -21,6 +21,20 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Project Nexus API",
+      default_version='v1',
+      description="Backend API for Polls, Options, and Votes",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,4 +42,8 @@ urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Swagger
+    path("swagger(<format>\.json|\.yaml)", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name="schema-redoc"),
 ]
