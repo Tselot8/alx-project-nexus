ec0928d2d60563fa8c8ac518b753c287e3a15626
Tselot Million <tselotmillion8@gmail.com>
2025-11-29 12:37:05 +0300

    feat: add register option

--- alx_project_nexus/urls.py
+++ alx_project_nexus/urls.py
@@ -39,6 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/polls/", include("polls.urls")),
+     path("api/accounts/", include("accounts.urls")),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
