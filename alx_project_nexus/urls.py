7c6ca9070159f7ee0ad72521363f8349769db4c1
Tselot Million <tselotmillion8@gmail.com>
2025-11-30 12:50:11 +0300

    fix: redirect to swagger

--- alx_project_nexus/urls.py
+++ alx_project_nexus/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include. re_path
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -24,6 +24,7 @@ from rest_framework_simplejwt.views import (
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.generic import RedirectView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -47,4 +48,5 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name="schema-redoc"),
+    path('', RedirectView.as_view(url='/swagger/', permanent=False)),
 ]
