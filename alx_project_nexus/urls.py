f7d8a769bec9b62f2632091d4fe4a06bf25709e0
Tselot Million <tselotmillion8@gmail.com>
2025-11-30 12:17:51 +0300

    fix: secret key naming

--- alx_project_nexus/urls.py
+++ alx_project_nexus/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include. re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -44,7 +44,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # Swagger
-    path(r"swagger(?P<format>\.json|\.yaml)", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name="schema-redoc"),
 ]
